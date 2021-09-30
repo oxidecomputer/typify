9ab9b707ae783b8109bd1937046d4d5566942115
Adam H. Leventhal <ahl@oxide.computer>
2021-09-30 11:36:03 -0700

    fix macro generation

--- typify-macro/src/lib.rs
+++ typify-macro/src/lib.rs
@@ -58,3 +58,44 @@ fn do_import_types(item: TokenStream) -> Result<TokenStream, syn::Error> {
 fn into_syn_err(e: typify_impl::Error, span: proc_macro2::Span) -> syn::Error {
     syn::Error::new(span, e.to_string())
 }
+
+fn do_import_types(item: TokenStream) -> Result<TokenStream, syn::Error> {
+    let arg = syn::parse::<LitStr>(item)?;
+    let dir = std::env::var("CARGO_MANIFEST_DIR").map_or_else(
+        |_| std::env::current_dir().unwrap(),
+        |s| Path::new(&s).to_path_buf(),
+    );
+
+    let path = dir.join(arg.value());
+
+    let content = match std::fs::read_to_string(path) {
+        Ok(s) => s,
+        Err(e) => {
+            return Err(syn::Error::new(
+                arg.span(),
+                format!("couldn't read file {}: {}", arg.value(), e.to_string()),
+            ));
+        }
+    };
+
+    let schema = serde_json::from_str::<schemars::schema::RootSchema>(&content).unwrap();
+
+    let mut type_space = TypeSpace::default();
+    type_space
+        .add_ref_types(schema.definitions)
+        .map_err(|e| into_syn_err(e, arg.span()))?;
+    let _ = type_space
+        .add_type(&Schema::Object(schema.schema))
+        .map_err(|e| into_syn_err(e, arg.span()))?;
+
+    let types = type_space.iter_types().map(|t| t.output(&type_space));
+
+    let file = quote! {
+        #(#types)*
+    };
+    Ok(file.into())
+}
+
+fn into_syn_err(e: typify_impl::Error, span: proc_macro2::Span) -> syn::Error {
+    syn::Error::new(span, e.to_string())
+}
