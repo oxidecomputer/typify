a5479b5b82b0dd0519cd3816525b75ba06120d88
Rick Richardson <rick@letter.co>
2022-05-26 16:03:37 -0700

    ensure that i64 is the default for integer, since i64 is the default type

--- typify-impl/src/defaults.rs
+++ typify-impl/src/defaults.rs
@@ -296,12 +296,12 @@ impl TypeEntry {
         let maybe_builtin = match &self.details {
             // This can only be covered by the intrinsic default
             TypeEntryDetails::Unit => unreachable!(),
-            TypeEntryDetails::Boolean => Some("defaults::default_bool::<false>".to_string()),
+            TypeEntryDetails::Boolean => Some("super::defaults::default_bool::<false>".to_string()),
             TypeEntryDetails::Integer(name) => {
-                if let Some(value) = default.as_u64() {
-                    Some(format!("defaults::default_u64::<{}, {}>", name, value))
-                } else if let Some(value) = default.as_i64() {
+                if let Some(value) = default.as_i64() {
                     Some(format!("defaults::default_i64::<{}, {}>", name, value))
+                } else if let Some(value) = default.as_u64() {
+                    Some(format!("defaults::default_u64::<{}, {}>", name, value))
                 } else if let Some(duration) = default.as_str() {
                     if let Ok(value) = humantime::parse_duration(duration) {
                         Some(format!("defaults::default_u64::<{}, {}>", name, value.as_secs()))
