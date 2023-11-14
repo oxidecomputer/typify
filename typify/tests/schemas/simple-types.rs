c9d6453fc3cf69726d539925b838b267f886cb53
Adam Leventhal <ahl@oxide.computer>
2023-11-14 08:54:51 -0800

    floats *are* real numbers too (#440)

---
+++ typify/tests/schemas/simple-types.rs
@@ -0,0 +1,13 @@
+#[allow(unused_imports)]
+use serde::{Deserialize, Serialize};
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct FloatsArentTerribleImTold {
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub flush_timeout: Option<f32>,
+}
+impl From<&FloatsArentTerribleImTold> for FloatsArentTerribleImTold {
+    fn from(value: &FloatsArentTerribleImTold) -> Self {
+        value.clone()
+    }
+}
+fn main() {}
