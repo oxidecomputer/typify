ce259cf3d9ee8914000ba241bc79907271d7ac9d
Adam H. Leventhal <ahl@oxide.computer>
2024-01-19 21:59:57 -0800

    new test

--- typify/tests/schemas/simple-types.rs
+++ typify/tests/schemas/simple-types.rs
@@ -52,4 +52,61 @@ impl From<&FloatsArentTerribleImTold> for FloatsArentTerribleImTold {
         value.clone()
     }
 }
+#[doc = "JustOne"]
+#[doc = r""]
+#[doc = r" <details><summary>JSON schema</summary>"]
+#[doc = r""]
+#[doc = r" ```json"]
+#[doc = "{"]
+#[doc = "  \"type\": ["]
+#[doc = "    \"string\""]
+#[doc = "  ]"]
+#[doc = "}"]
+#[doc = r" ```"]
+#[doc = r" </details>"]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+pub enum JustOne {
+    Variant0(String),
+}
+impl From<&JustOne> for JustOne {
+    fn from(value: &JustOne) -> Self {
+        value.clone()
+    }
+}
+impl std::str::FromStr for JustOne {
+    type Err = self::error::ConversionError;
+    fn from_str(value: &str) -> Result<Self, self::error::ConversionError> {
+        if let Ok(v) = value.parse() {
+            Ok(Self::Variant0(v))
+        } else {
+            Err("string conversion failed for all variants".into())
+        }
+    }
+}
+impl std::convert::TryFrom<&str> for JustOne {
+    type Error = self::error::ConversionError;
+    fn try_from(value: &str) -> Result<Self, self::error::ConversionError> {
+        value.parse()
+    }
+}
+impl std::convert::TryFrom<&String> for JustOne {
+    type Error = self::error::ConversionError;
+    fn try_from(value: &String) -> Result<Self, self::error::ConversionError> {
+        value.parse()
+    }
+}
+impl std::convert::TryFrom<String> for JustOne {
+    type Error = self::error::ConversionError;
+    fn try_from(value: String) -> Result<Self, self::error::ConversionError> {
+        value.parse()
+    }
+}
+impl ToString for JustOne {
+    fn to_string(&self) -> String {
+        match self {
+            Self::Variant0(x) => x.to_string(),
+        }
+    }
+}
 fn main() {}
